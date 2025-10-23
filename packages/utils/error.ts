d38cee0859cfb0b133df5f7d6f2179caaae9bad6
Jonnyaaa <3416998350@qq.com>
2025-10-23 18:27:35 +0800

    refactor: 抽离 utils/error 中生成 JoUIError 的逻辑

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -8,10 +8,14 @@ class JoUIError extends Error {
   }
 }
 
+function createJoUIError(scope: string, msg: string) {
+  return new JoUIError(`[${scope}]:${msg}`);
+}
+
 // scope：错误发生的范围（如组件名 JoCollapse、工具函数名等），用于定位错误来源
 // msg：具体的错误描述
 export function throwError(scope: string, msg: string) {
-  throw new JoUIError(`[${scope}]:${msg}`);
+  throw createJoUIError(scope, msg);
 }
 
 // 函数重载声明：支持两种调用方式
@@ -21,7 +25,7 @@ export function debugWarn(scope: string, msg: string): void;
 export function debugWarn(scope: string | Error, msg?: string) {
   if (process.env.NODE_ENV !== "production") { // 仅在非生产环境执行
     // 若 scope 是字符串，生成新的 JoUIError,若 scope 是 Error 对象，直接使用
-    const err = isString(scope) ? new JoUIError(`[${scope}]:${msg}`) : scope;
+    const err = isString(scope) ? createJoUIError(scope, msg!) : scope;
     console.warn(err);
   }
 }
