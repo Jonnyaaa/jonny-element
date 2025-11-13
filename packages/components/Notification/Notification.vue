b8401b36784b8012acf0d168480cae42724a37d9
Jonnyaaa <3416998350@qq.com>
2025-11-13 14:48:07 +0800

    feat: Notification Position

--- packages/components/Notification/Notification.vue
+++ packages/components/Notification/Notification.vue
@@ -14,6 +14,7 @@ const props = withDefaults(defineProps<NotificationProps>(), {
   type: "info",
   duration: 3000,
   offset: 20,
+  position: "top-right",
   transitionName: "fade",
   showClose: true,
 });
@@ -31,8 +32,16 @@ const { topOffset, bottomOffset } = useOffset({
 
 const iconName = computed(() => typeIconMap.get(props.type) ?? "circle-info");
 
+const horizontalClass = computed(() =>
+  props.position.endsWith("right") ? "right" : "left"
+);
+
+const verticalProperty = computed(() =>
+  props.position.startsWith("top") ? "top" : "bottom"
+);
+
 const customStyle = computed(() => ({
-  top: addUnit(topOffset.value),
+  [verticalProperty.value]: addUnit(topOffset.value),
   zIndex: props.zIndex,
 }));
 
@@ -71,7 +80,7 @@ defineExpose<NotificationCompInstance>({
       ref="notifyRef"
       class="jo-notification"
       :class="{
-        [`jo-notification--${type}`]: type,
+        [horizontalClass]: true,
         'show-close': showClose,
       }"
       :style="customStyle"
